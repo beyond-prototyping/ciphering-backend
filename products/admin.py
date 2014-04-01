f112f51c12fe311b75aa3b36b3e1efda9fd0fdf8
Philipp Bosch <hello@pb.io>
2014-04-01 15:42:37 +0200

    Better orders admin.

--- products/admin.py
+++ products/admin.py
@@ -3,7 +3,12 @@ from .models import Order
 
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('uuid', 'digits',)
+    list_display = ('uuid', 'digits', 'email', 'status', 'created', 'shapeways_link', )
     readonly_fields = ('uuid', 'status', 'params', 'digits', 'material', 'email', 'compile_job_id', 'upload_job_id', 'email_job_id', 'shapeways_url', 'created', 'modified',)
+
+    def shapeways_link(self, instance):
+        return u'<a href="{0}">{0}</a>'.format(instance.shapeways_url)
+    shapeways_link.short_description = 'Shapeways Link'
+    shapeways_link.allow_tags = True
 
 admin.site.register(Order, OrderAdmin)
